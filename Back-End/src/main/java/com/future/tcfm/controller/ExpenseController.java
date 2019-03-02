5250fb62866e3284d5623b3efbacf791997794c1
Habib <mhabibofficial@yahoo.com>
2019-03-02 17:21:34 +0700

    #5 update

--- Back-End/src/main/java/com/future/tcfm/controller/ExpenseController.java
+++ Back-End/src/main/java/com/future/tcfm/controller/ExpenseController.java
@@ -1,8 +1,38 @@
 package com.future.tcfm.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.future.tcfm.model.Expense;
+import com.future.tcfm.model.Expense;
+import com.future.tcfm.service.ExpenseService;
+import com.future.tcfm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
+@CrossOrigin
 @RestController
+@RequestMapping("/api")
 public class ExpenseController {
+
+    @Autowired
+    ExpenseService expenseService;
+
+    @GetMapping("/expense")
+    public List<Expense> loadAll (){
+        return expenseService.loadAll();
+    }
+
+    @PostMapping("/expense")
+    public ResponseEntity createExpense(@RequestBody Expense expense) {
+        return expenseService.createExpense(expense);
+    }
+
+    @PutMapping("/expense/{id}")
+    public ResponseEntity<Expense> updateExpense(@PathVariable("id") String id, @RequestBody Expense expense) {
+        return expenseService.updateExpense(id,expense);
+    }
 }
+
+
--- Back-End/src/main/java/com/future/tcfm/service/impl/ExpenseServiceImpl.java
+++ Back-End/src/main/java/com/future/tcfm/service/impl/ExpenseServiceImpl.java
@@ -1,8 +1,31 @@
 package com.future.tcfm.service.impl;
 
+import com.future.tcfm.model.Expense;
+import com.future.tcfm.repository.ExpenseRepository;
 import com.future.tcfm.service.ExpenseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ExpenseServiceImpl implements ExpenseService {
+    @Autowired
+    ExpenseRepository expenseRepository;
+
+    @Override
+    public List<Expense> loadAll() {
+        return expenseRepository.findAll();
+    }
+
+    @Override
+    public ResponseEntity createExpense(Expense expense) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity updateExpense(String id, Expense expense) {
+        return null;
+    }
 }
