06e9e759b193f4a987ae3462cc4844e677834a88
Habib <mhabibofficial@yahoo.com>
2019-05-04 15:40:01 +0700

    update

--- Front-End/src/page/home.vue
+++ Front-End/src/page/home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <router-link to="/login">
-        Login
+          Login
         </router-link>
       <button @click.prevent="logout">Logout</button>
       <button @click.prevent="hello">Tes Hello</button>
@@ -11,26 +11,19 @@
 <script>
   import { mapActions } from 'vuex'
 
-export default {
-
-  methods : {
-    ...mapActions([
-       'logout'
-     ]),
-    hello() {
-      const auth={
-        headers:{'Authorization':localStorage.getItem('token')}}
-        //https://stackoverflow.com/questions/3102819/disable-same-origin-policy-in-chrome
-      this.axios
-        .get('http://localhost:8088/api/hello',auth)
-        .then((res) => {
-            alert (JSON.stringify(res))
-        }).catch((e) => {
-          alert(e)
-      })
+  export default {
+    methods : {
+      ...mapActions([
+         'logout'
+       ]),
+      hello() {
+        fetch('/api/hello',{
+          headers: {'Authorization': localStorage.getItem('token')}
+        })
+          .then(response => console.log(response))
+      }
     }
   }
-}
 </script>
 
 <style>
