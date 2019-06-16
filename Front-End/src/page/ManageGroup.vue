573519afa8d17ba15ae371392d10b9c21c2ce295
Robin <robin071196@gmail.com>
2019-06-16 16:16:55 +0700

    Manage User Page

---
+++ Front-End/src/page/ManageGroup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class='manageGroupComponent'>
+    <SidebarComponent />
+
+    <div class='rightPanel' :style="{ width: rightPanelWidth + 'px' }">
+      <HeaderSection headerTitle='Manage Group'/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import SidebarComponent from '../components/Sidebar';
+  import HeaderSection from '../components/HeaderSection';
+
+  export default {
+    computed: {
+      rightPanelWidth: function() {
+        return (document.documentElement.clientWidth - 280);
+      }
+    }
+  }
+</script>
+
+<style>
+  .manageGroupComponent {
+    display: flex;
+  }
+
+  .rightPanel {
+    padding: 20px 20px 0 30px;
+    box-sizing: border-box;
+  }
+</style>
