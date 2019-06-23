54bc74e79c6c72d0b16f2eff2380c78b6a619864
azhang0411 <azhang0411@yahoo.com>
2019-06-24 01:59:29 +0700

    Added security changes

--- Back-End/src/main/java/com/future/tcfm/config/security/JwtValidator.java
+++ Back-End/src/main/java/com/future/tcfm/config/security/JwtValidator.java
@@ -1,32 +1,40 @@
 package com.future.tcfm.config.security;
 
 import com.future.tcfm.model.User;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class JwtValidator {
 
-
-    private String secret = "futureProgram";
+    @Value("${app.jwtSecret}")
+    private String secretKey;
 
     public User validate(String token) {
 
         User user= null;
         try {
             Claims body = Jwts.parser()
-                    .setSigningKey(secret)
+                    .setSigningKey(secretKey)
                     .parseClaimsJws(token)
                     .getBody();
 
             user = new User();
             user.setEmail(body.getSubject());
             user.setPassword((String) body.get("password"));
-        }
-        catch (Exception e) {
-            System.out.println(e);
+            user.setRole((String)body.get("role"));
+        } catch (SignatureException ex) {
+            throw new RuntimeException("Invalid JWT signature");
+        } catch (MalformedJwtException ex) {
+            throw new RuntimeException("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            throw new RuntimeException("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            throw new RuntimeException("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            throw new RuntimeException("JWT Claims String is empty");
         }
 
         return user;
