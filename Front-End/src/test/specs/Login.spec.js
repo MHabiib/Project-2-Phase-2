a03c75d4e1a2ae125e5214baff620d03afbd8c74
azhang0411 <azhang0411@yahoo.com>
2019-08-14 15:50:28 +0700

    -switched to dev2

---
+++ Front-End/src/test/specs/Login.spec.js
@@ -0,0 +1,13 @@
+
+// import { mount } from '@vue/test-utils'
+// import Login from '../../page/login.vue'
+// const mount = require('@vue/test-utils')
+// const Login = require('../../page/login.vue')
+
+describe('Login.vue',() =>{
+    test('setup correctly', ()=>{
+        // const wrapper = mount(Login)
+        // expect(wrapper.text()).toContain('Login')
+        expect(true).toBe(true)
+    })
+})
